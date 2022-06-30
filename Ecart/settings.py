598505ee7bce29eb594061d8cd09fb702bb66b0d
AtishKumar98 <atishkumar31518@gmail.com>
2022-06-30 18:11:21 +0530

    initial-6 commit

--- Ecart/settings.py
+++ Ecart/settings.py
@@ -197,7 +197,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'vickyjoohn519@gmail.com'
-EMAIL_HOST_PASSWORD = 'atish@56985698'
+EMAIL_HOST_PASSWORD = 'bing'
 
 
 
