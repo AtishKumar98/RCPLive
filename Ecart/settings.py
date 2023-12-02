dc80a55130074604f3e680b0a8d508c07296e065
AtishKumar98 <atishkumar31518@gmail.com>
2023-12-02 10:23:26 +0530

    settingsfile

--- Ecart/settings.py
+++ Ecart/settings.py
@@ -110,23 +110,23 @@ WSGI_APPLICATION = 'Ecart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'Smedia',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Jett@56325632',
-#         'HOST':'localhost',
-#         'PORT':'5432',
-#     }
-# }
-
 DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Smedia',
+        'USER': 'postgres',
+        'PASSWORD': 'Jett@56325632',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
-}   
+}
+
+# DATABASES = {
+#         'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }   
 
 
 
